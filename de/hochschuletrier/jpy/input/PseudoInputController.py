32d8b042da1fa54de5b1b4671b6663a40d1cc0e6
Johannes <kirchnej@hochschule-trier.de>
2015-04-28 15:44:36 +0200

    fixed something more

--- de/hochschuletrier/jpy/input/PseudoInputController.py
+++ de/hochschuletrier/jpy/input/PseudoInputController.py
@@ -5,7 +5,8 @@ from de.hochschuletrier.jpy.input.InputController import InputController
 
 
 class PseudoInputController(InputController):
-    def __init__(self,root):
+    def __init__(self, root):
+        self.root = root
         self.logger = JPYLogger(self)
         self.logger.log("PseudoInputController initialisiert")
         self.blockBuzzer = False
@@ -16,7 +17,7 @@ class PseudoInputController(InputController):
         mainWindow.bind("<KP_2>", lambda event: self.test(event, 2))
         mainWindow.bind("<KP_Add>", self.addPoints)
         mainWindow.bind("<KP_Subtract>", self.subPoints)
-        #mainWindow.bind("p", self.userName)
+        # mainWindow.bind("p", self.userName)
         mainWindow.bind("9", self.startGame)
         mainWindow.bind("1", lambda event: self.pressedBuzzer(event, 0))
         mainWindow.bind("2", lambda event: self.pressedBuzzer(event, 1))
