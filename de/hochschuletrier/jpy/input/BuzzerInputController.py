304299e17472824a7ca0122dc22f715cc179f97e
Johannes <kirchnej@hochschule-trier.de>
2015-04-30 11:14:43 +0200

    threaded wait_for_interrupts

--- de/hochschuletrier/jpy/input/BuzzerInputController.py
+++ de/hochschuletrier/jpy/input/BuzzerInputController.py
@@ -3,6 +3,7 @@ import sys
 from de.hochschuletrier.jpy.console.JPYLogger import JPYLogger
 from de.hochschuletrier.jpy.input.InputController import InputController
 import RPIO
+from thread import start_new_thread
 
 
 class BuzzerInputController(InputController):
@@ -14,5 +15,9 @@ class BuzzerInputController(InputController):
         mainWindow = root.mainWindow
         RPIO.add_interrupt_callback(18, lambda x, y: self.pressedBuzzer(trigger=0), edge='rising',
                                     pull_up_down=RPIO.PUD_UP, threaded_callback=True)
+        start_new_thread(self.blocking_wait_for_interrupts, self)
+
+    def blocking_wait_for_interrupts(self):
         while (True):
-            RPIO.wait_for_interrupts(threaded=True)
+            print 'waiting for interrupts'
+            RPIO.wait_for_interrupts(threaded=False)
