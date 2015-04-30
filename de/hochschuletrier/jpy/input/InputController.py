9f08b1c4c7ed018323c1464f6e06fb9e0a9df737
Johannes <kirchnej@hochschule-trier.de>
2015-04-30 14:06:53 +0200

    ugly

--- de/hochschuletrier/jpy/input/InputController.py
+++ de/hochschuletrier/jpy/input/InputController.py
@@ -4,11 +4,13 @@ from de.hochschuletrier.jpy.console.JPYLogger import JPYLogger
 
 
 class InputController:
+    blockBuzzer = False
+
     def __init__(self, root):
         self.root = root
         self.logger = JPYLogger(self)
         self.logger.log("Input controller initialisiert")
-        self.blockBuzzer = False
+
         mainWindow = root.mainWindow
 
 
@@ -29,13 +31,13 @@ class InputController:
     # def pressedBuzzer(self, event, trigger):
     def pressedBuzzer(self, trigger):
         print 'blocked=', self.blockBuzzer
-        if self.blockBuzzer:
+        if InputController.blockBuzzer:
             return
         if self.root.gameStateManager.activeState == 0:
             return
         if not self.root.gameStarted:
             # ENTER USERNAME SUBROUTINE
-            self.blockBuzzer = True
+            InputController.blockBuzzer = True
             self.logger.log(str(trigger) + " enter Name")
             self.userName(trigger)
         if not self.root.gameStateManager.states[1].overlayManager.overlays[0].isVisible:
@@ -46,7 +48,7 @@ class InputController:
             self.root.gameStateManager.states[1].overlayManager.overlays[0].highlight(
                 self.root.questionManager.candidate.color)
             self.logger.prompt("Candidate :: " + str(trigger) + " ::  pressed Buzzer")
-            self.blockBuzzer = True
+            InputController.blockBuzzer = True
             self.root.audioManager.pause()
 
     def endProgram(self, event):
@@ -70,7 +72,7 @@ class InputController:
         self.root.questionManager.candidate = None
         self.root.gameStateManager.states[1].overlayManager.overlays[0].normalize()
         # self.root.gameStateManager.states[1].overlayManager.overlays[0].hide(self)
-        self.blockBuzzer = False
+        InputController.blockBuzzer = False
         self.root.audioManager.resumeBackgroundSong()
 
     def addPoints(self, event):
@@ -83,5 +85,5 @@ class InputController:
         self.root.questionManager.candidate.addPoints(self.root.questionManager.worth)
         self.root.questionManager.candidate = None
         self.root.gameStateManager.states[1].overlayManager.overlays[0].hide(self)
-        self.blockBuzzer = False
+        InputController.blockBuzzer = False
         self.root.audioManager.stop()
