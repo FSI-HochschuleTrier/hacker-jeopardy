45b03ea77d6ce22637c4b74888eb589ad1b06431
Johannes <kirchnej@hochschule-trier.de>
2015-04-30 13:01:23 +0200

    another debug commit

--- de/hochschuletrier/jpy/input/BuzzerInputController.py
+++ de/hochschuletrier/jpy/input/BuzzerInputController.py
@@ -3,7 +3,6 @@ import sys
 from de.hochschuletrier.jpy.console.JPYLogger import JPYLogger
 from de.hochschuletrier.jpy.input.InputController import InputController
 import RPIO
-from thread import start_new_thread
 
 
 class BuzzerInputController(InputController):
@@ -15,10 +14,5 @@ class BuzzerInputController(InputController):
         mainWindow = root.mainWindow
         RPIO.add_interrupt_callback(18, lambda x, y: self.pressedBuzzer(trigger=0), edge='rising',
                                     pull_up_down=RPIO.PUD_UP, threaded_callback=True)
-        start_new_thread(self.blocking_wait_for_interrupts,())
 
-    def blocking_wait_for_interrupts(self):
-        while (True):
-            print 'waiting for interrupts'
-            RPIO.wait_for_interrupts(threaded=False)
-            print 'buzzed?'
+        RPIO.wait_for_interrupts(threaded=True)
