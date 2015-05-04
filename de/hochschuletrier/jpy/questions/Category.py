af88b3aa6df874c4a336a6eaf01367bd140bd3de
MikO <miko007@me.com>
2015-05-04 17:30:21 +0200

    fixed encoding problems

--- de/hochschuletrier/jpy/questions/Category.py
+++ de/hochschuletrier/jpy/questions/Category.py
@@ -7,5 +7,5 @@ class Category:
         self.answers = []
 
     def addQuestion(self, question, answer):
-        self.questions.append(str(question))
-        self.answers.append(str(answer))
+        self.questions.append(question.encode("utf-8"))
+        self.answers.append(answer.encode("utf-8"))
