c54376fcd83af73a2535279e26255c5ec72b0022
Georg Schäfer <georg.peter.schaefer@gmail.com>
2015-06-25 17:37:44 +0200

    fix gpio

--- de/hochschuletrier/jpy/input/BuzzerInputController.py
+++ de/hochschuletrier/jpy/input/BuzzerInputController.py
@@ -12,9 +12,11 @@ class BuzzerInputController(InputController):
         self.logger.log("BuzzerInputController initialisiert")
         self.blockBuzzer = False
         mainWindow = root.mainWindow
-        RPIO.add_interrupt_callback(18, lambda x, y: self.pressedBuzzer(trigger=0), edge='rising',
+        RPIO.add_interrupt_callback(25, lambda x, y: self.pressedBuzzer(trigger=0), edge='rising',
                                     pull_up_down=RPIO.PUD_UP)
-        RPIO.add_interrupt_callback(25, lambda x, y: self.pressedBuzzer(trigger=1), edge='rising',
+        RPIO.add_interrupt_callback(8, lambda x, y: self.pressedBuzzer(trigger=1), edge='rising',
                                     pull_up_down=RPIO.PUD_UP)
+        RPIO.add_interrupt_callback(7, lambda x, y: self.pressedBuzzer(trigger=1), edge='rising',
+                            pull_up_down=RPIO.PUD_UP)
 
         RPIO.wait_for_interrupts(threaded=True)
