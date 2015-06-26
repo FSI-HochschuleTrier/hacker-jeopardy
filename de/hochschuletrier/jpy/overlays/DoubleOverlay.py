061c367a47876b2f114f6b79d503ce370e3499e0
Georg Schäfer <georg.peter.schaefer@gmail.com>
2015-06-26 22:54:09 +0200

    fix double jeopardy size

--- de/hochschuletrier/jpy/overlays/DoubleOverlay.py
+++ de/hochschuletrier/jpy/overlays/DoubleOverlay.py
@@ -16,7 +16,7 @@ class DoubleOverlay(Overlay):
             bd=4,
             background="gold"
         )
-        self.place(x=0, y=0, relwidth=1, relheight=1, width=-1000, height=-550)
+        self.place(x=0, y=0, relwidth=1, relheight=1, width=-2000, height=-1100)
 
         self.renderLabel()
 
