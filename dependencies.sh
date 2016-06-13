f8671f27567a89d49e4178b111a431ce9466759c
Michael Ochmann <miko007@me.com>
2016-06-13 15:58:44 +0200

    improved dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -5,10 +5,11 @@ sudo apt-get install python2.7
 git clone https://github.com/FSI-HochschuleTrier/mplayer.py.git
 cd mplayer.py
 mv mplayer ../mplayer
-git https://github.com/tylerwowen/RPIO.git
+cd ..
+sudo rm -rf mplayer.py
+git clone https://github.com/tylerwowen/RPIO.git
 cd RPIO
 sudo python setup.py install
 cd ..
-sudo rm -rf mplayer.py
 sudo rm -rf RPIO
-cp resources/swiss-911-ultra-compressed-bt.ttf ~/.fonts/
+mkdir -p ~/.fonts
